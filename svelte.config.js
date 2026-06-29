0057108bcb45aa306cd2506b1ed61262792f7ab2
Engy Park <a01071397193@gmail.com>
2026-06-29 17:09:06 +0900

    chore: scaffold static SvelteKit app

---
+++ svelte.config.js
@@ -0,0 +1,20 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const base = process.env.BASE_PATH ?? '';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      fallback: '404.html',
+      strict: false
+    }),
+    paths: {
+      base
+    }
+  }
+};
+
+export default config;
